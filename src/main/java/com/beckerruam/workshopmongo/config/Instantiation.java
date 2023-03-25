34b50143a7a1908ff0853f77055529ee1c353ef4
Ruam Becker <becker.ruam@gmail.com>
2023-03-24 22:16:43 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/beckerruam/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.beckerruam.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.beckerruam.workshopmongo.domain.User;
+import com.beckerruam.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
