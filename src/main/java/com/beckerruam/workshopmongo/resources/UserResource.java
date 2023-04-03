4e09ffe050e2c7d5e835149cc8a5eb2ae918da4d
Ruam Becker <becker.ruam@gmail.com>
2023-04-03 12:54:17 -0300

    Obtendo um usuario por id

--- src/main/java/com/beckerruam/workshopmongo/resources/UserResource.java
+++ src/main/java/com/beckerruam/workshopmongo/resources/UserResource.java
@@ -6,7 +6,9 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beckerruam.workshopmongo.domain.User;
@@ -26,4 +28,10 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findByID(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
 }
