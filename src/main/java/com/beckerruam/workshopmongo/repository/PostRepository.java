bd49f10d1092307a232f88f30ed9910b0aa3105e
Ruam Becker <becker.ruam@gmail.com>
2023-06-16 12:35:58 -0300

    Consulta simples com @Query

--- src/main/java/com/beckerruam/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/beckerruam/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.beckerruam.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.beckerruam.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") //?0 primeiro parametro recebido no metodo
+	List<Post> searchTitle (String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
